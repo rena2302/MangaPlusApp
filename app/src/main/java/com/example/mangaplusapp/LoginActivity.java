3707f4f762341651182e41a3382422abba81258a
YunneToiChoi <phanvannang004@gmail.com>
2024-01-02 13:25:52 +0700

    1/2 event for login back and register back

--- app/src/main/java/com/example/mangaplusapp/LoginActivity.java
+++ app/src/main/java/com/example/mangaplusapp/LoginActivity.java
@@ -2,7 +2,10 @@ package com.example.mangaplusapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        TextView toSignUp = findViewById(R.id.toSignUp);
+        toSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class); // bat su kien
+                startActivity(intent); // chay su kien
+            }
+        });
     }
 }
