69654b932f35c3b9f4f5c75452423935433937b9
YunneToiChoi <phanvannang004@gmail.com>
2024-01-06 18:00:04 +0700

    Finished BACK END FRONT END LOGIN AND REGISTER

--- app/src/main/java/com/example/mangaplusapp/LoginActivity.java
+++ app/src/main/java/com/example/mangaplusapp/LoginActivity.java
@@ -4,21 +4,67 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import Database.CreateDatabase;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText email, password;
+    Button btnLogin;
+    CreateDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //lay id
         setContentView(R.layout.activity_login);
         TextView toSignUp = findViewById(R.id.toSignUp);
+        //lay id
         toSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class); // bat su kien
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class); // bat su kien
                 startActivity(intent); // chay su kien
+            }
+        });
+
+        //database
+        // CONNECT DATABASE
+        db = new CreateDatabase(this);
+        db.open();
+        //BEGIN LOGIC LOGIN
+        email = (EditText) findViewById(R.id.editTextEmail);
+        password = (EditText) findViewById(R.id.editTextPassword);
+        btnLogin = (Button) findViewById(R.id.loginBtn_act);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userEmail = email.getText().toString();
+                String userPassword = password.getText().toString();
+                // if user do nothing or Missing input
+                if(userEmail.equals("")||userPassword.equals("")){
+                    Toast.makeText(LoginActivity.this,"Please enter all fields", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Boolean checkEmailPass= db.CheckEmailPassword(userEmail,userPassword);
+                    // neu email va password hop le -> ra home activity
+                    if(checkEmailPass){
+
+                        Toast.makeText(LoginActivity.this,"Sign Ip Successfully", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                        startActivity(intent);
+                    }
+                    // thong bao cac truong khong hop le
+                    else{
+                        Toast.makeText(LoginActivity.this,"Invalid Credentials", Toast.LENGTH_SHORT).show();
+                    }
+                }
             }
         });
     }
--- app/src/main/java/com/example/mangaplusapp/RegisterActivity.java
+++ app/src/main/java/com/example/mangaplusapp/RegisterActivity.java
@@ -5,14 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
 import Database.CreateDatabase;
 
 public class RegisterActivity extends AppCompatActivity {
+    EditText name,email,password,repassword;
+    Button signUp;
+    CreateDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +28,6 @@ public class RegisterActivity extends AppCompatActivity {
         TextView toLogin=findViewById(R.id.toLogin);
         ImageButton btnToLogin = findViewById(R.id.backRegisterBtn);
         // LAY ID
-
-        // CONNECT DATABASE
-        CreateDatabase db = new CreateDatabase(this);
-        db.open();
-
         toLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -41,6 +42,67 @@ public class RegisterActivity extends AppCompatActivity {
                 // CHUYEN LAYOUT
                 Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
                 startActivity(intent);
+            }
+        });
+
+
+        // CONNECT DATABASE
+        db= new CreateDatabase(this);
+        db.open();
+        //BEGIN LOGIC REGISTER
+        name=(EditText) findViewById(R.id.editTextName);
+        email= (EditText) findViewById(R.id.editTextEmail);
+        password = (EditText) findViewById(R.id.editTextPassword);
+        repassword = (EditText) findViewById(R.id.editTextCfPassword);
+        signUp = (Button) findViewById(R.id.registerBtn_act);
+        db= new CreateDatabase(this);
+
+        // su kien dang ky
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userName = name.getText().toString();
+                String userEmail = email.getText().toString();
+                String userPassword = password.getText().toString();
+                String userCfPassword = repassword.getText().toString();
+
+                // neu may thg input == null
+                if(userEmail.equals("")|| userPassword.equals("") || userName.equals("") || userCfPassword.equals("")){
+                    Toast.makeText(RegisterActivity.this,"Please enter all the fields",Toast.LENGTH_SHORT).show();
+
+                }
+                else{
+                    // neu pass match voi cf pass
+                    if(userPassword.equals(userCfPassword)){
+                        Boolean checkEmail = db.CheckEmail(userEmail);
+                        if(checkEmail==false){ // neu email khong ton tai trong he thong
+                            Boolean insert = db.insertData(userEmail,userPassword);
+
+                            if(insert) {
+                                // thong bao thanh cong
+                                Toast.makeText(RegisterActivity.this, "Registered successfully", Toast.LENGTH_SHORT).show();
+                                // chuyen sang home layout
+                                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                                startActivity(intent);
+                            }
+                            // neu insert thanh cong
+                            else{
+                                // thong bao that bai
+                                Toast.makeText(RegisterActivity.this, "Registered Failed", Toast.LENGTH_SHORT).show();
+                                // do nothing
+                            }
+                        }
+                        else{
+                            // thong bao da co email trong he thong, vui long dang nhap
+                            Toast.makeText(RegisterActivity.this, "User already exists ! Please sign in", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                    else{
+                        Toast.makeText(RegisterActivity.this, "Password not match, please try again", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
             }
         });
     }
