11a76ebada848a342756305769fde823a89b9707
Phan Văn Năng <109004626+YunneeToiChoi@users.noreply.github.com>
2024-02-05 17:08:35 +0700

    layout user Profile upgrade : set text for user

--- app/src/main/java/com/example/mangaplusapp/UserProfileFragment.java
+++ app/src/main/java/com/example/mangaplusapp/UserProfileFragment.java
@@ -1,6 +1,9 @@
 package com.example.mangaplusapp;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -11,15 +14,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+
+import Database.CreateDatabase;
 
 public class UserProfileFragment extends Fragment {
 Button btnTest;
+CreateDatabase db;
+TextView getUserNameInfoTxt,getUserNameTittleTxt,getUserEmailTxt,getUserPasswordTxt;
+int userId;
+String userEmail,userPassword,userName;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
+    @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -27,8 +38,32 @@ Button btnTest;
         View root = inflater.inflate(R.layout.fragment_user_profile, container,false);
         btnTest = root.findViewById(R.id.navToEditProfile);
         navigateLayout();
+        //=========================================Get id=========================================//
+        getUserNameInfoTxt = root.findViewById(R.id.userName_info);
+        getUserNameTittleTxt = root.findViewById(R.id.userName_Tittle);
+        getUserEmailTxt = root.findViewById(R.id.userEmail_info);
+        getUserPasswordTxt = root.findViewById(R.id.userPassword_info);
+        //****************************************************************************************//
+        //=========================================Get data=======================================//
+        db = new CreateDatabase(requireContext());
+        SharedPreferences preferences = getContext().getSharedPreferences("user_session", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        userId= preferences.getInt("user_id",-1);
+        userName = db.getUserName(userId);
+        userPassword = db.getUserPassword(userId);
+        userEmail = db.getUserEmail(userId);
+        //****************************************************************************************//
+        //=========================================Get views======================================//
+        if(userId!=-1){
+            userExists();
+        }
+        else{
+            userExists();
+        }
+        //****************************************************************************************//
         return root;
     }
+
     private void loadFragment(Fragment fragment, boolean isAppInitialized) {
         FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
@@ -40,6 +75,18 @@ Button btnTest;
             fragmentTransaction.addToBackStack(fragment.getClass().getSimpleName());
         }
         fragmentTransaction.commit();
+    }
+    void userExists(){
+        getUserEmailTxt.setText(userEmail);
+        getUserPasswordTxt.setText("chac kh de len dau");
+        getUserNameInfoTxt.setText(userName);
+        getUserNameTittleTxt.setText(userName);
+    }
+    void userNotExists(){
+        getUserEmailTxt.setText("Guest");
+        getUserPasswordTxt.setText("Guest");
+        getUserNameInfoTxt.setText("Guest");
+        // log out if want
     }
     void navigateLayout(){
         btnTest.setOnClickListener(v->{
