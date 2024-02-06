e0889141375a73f6b7980e2ec4519bb3679dab8c
Phan Văn Năng <109004626+YunneeToiChoi@users.noreply.github.com>
2024-02-06 20:19:00 +0700

    forgot base layout

--- app/src/main/java/com/example/mangaplusapp/ForgotFragment.java
+++ app/src/main/java/com/example/mangaplusapp/ForgotFragment.java
@@ -1,17 +1,26 @@
 package com.example.mangaplusapp;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import Database.CreateDatabase;
+
 public class ForgotFragment extends Fragment {
     Button SendOtpBtn;
+    String userEmail;
+    CreateDatabase db;
+    int userID;
+    EditText getUserEmailTxt;
     public ForgotFragment() {
         // Required empty public constructor
     }
@@ -27,8 +36,21 @@ public class ForgotFragment extends Fragment {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_forgot, container, false);
         SendOtpBtn = root.findViewById(R.id.btnForgotSendOtp);
+        //========================================GET ID==========================================//
+        getUserEmailTxt=root.findViewById(R.id.forgotEmailUser_Input);
+        //****************************************************************************************//
+        //=====================================GET DATABASE=======================================//
+        db=new CreateDatabase(getContext());
+        SharedPreferences preferences = getContext().getSharedPreferences("user_session", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor=preferences.edit();
+        userID=preferences.getInt("user_id",-1);
+        userEmail=db.getUserEmail(userID);
+        //****************************************************************************************//
         //==================================NAVIGATE LAYOUT=======================================//
         navigateLayout();
+        //****************************************************************************************//
+        //=======================================SEND OTP=========================================//
+
         //****************************************************************************************//
         return root;
     }
