ddeb4ce2d983283c30bf9bb68525c4a574308b7e
rena2302 <khoivu2302@gmail.com>
2024-02-22 15:06:01 +0700

    request http

--- app/src/main/java/com/example/mangaplusapp/PaymentActivity.java
+++ app/src/main/java/com/example/mangaplusapp/PaymentActivity.java
@@ -12,9 +12,17 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
+
+import javax.crypto.Mac;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -24,6 +32,8 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
+import okio.ByteString;
+import okio.HashingSink;
 import vn.momo.momo_partner.AppMoMoLib;
 import vn.momo.momo_partner.MoMoParameterNameMap;
 import vn.momo.momo_partner.MoMoParameterNamePayment;
@@ -59,6 +69,7 @@ public class PaymentActivity extends Activity {
             }
         });
     }
+    /*------------------------------BEGIN-----------------------------------------*/
     private void initView(){
         tvEnvironment = (TextView) findViewById(R.id.tvEnvironment);
         tvMerchantCode = (TextView) findViewById(R.id.tvMerchantCode);
@@ -68,6 +79,9 @@ public class PaymentActivity extends Activity {
         btnPayMoMo = (Button) findViewById(R.id.btnPayMoMo);
     }
     //example payment
+    /*------------------------------END-----------------------------------------*/
+
+    /*------------------------------BEGIN-----------------------------------------*/
     private void requestPayment() {
         AppMoMoLib.getInstance().setAction(AppMoMoLib.ACTION.PAYMENT);
         AppMoMoLib.getInstance().setActionType(AppMoMoLib.ACTION_TYPE.GET_TOKEN);
@@ -103,48 +117,9 @@ public class PaymentActivity extends Activity {
         //Request momo app
         AppMoMoLib.getInstance().requestMoMoCallBack(this, eventValue);
     }
-    private void sendPaymentInfoToServer() {
-        // Tạo một yêu cầu HTTP POST để gửi thông tin đến máy chủ của bạn
+    /*------------------------------END-----------------------------------------*/
 
-        OkHttpClient client = new OkHttpClient();
-        MediaType mediaType = MediaType.parse("application/json; charset=UTF-8");
-        JSONObject jsonObject = new JSONObject();
-        try {
-            // Đặt các thuộc tính cho đối tượng JSON
-            jsonObject.put("partnerCode", eventValue.get(MoMoParameterNamePayment.PARTNER_CODE));
-            jsonObject.put("requestId", eventValue.get(MoMoParameterNamePayment.REQUEST_ID));
-            jsonObject.put("amount", eventValue.get(MoMoParameterNamePayment.AMOUNT));
-            jsonObject.put("orderId", eventValue.get("orderId"));
-            jsonObject.put("orderInfo", "Rau ma");
-            jsonObject.put("requestType", "captureWallet");
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        Log.d("requestCode", "sendPaymentInfoToServer: " + jsonObject.toString());
-        RequestBody body = RequestBody.create(mediaType, jsonObject.toString());
-        Request request = new Request.Builder()
-                .url("https://test-payment.momo.vn/v2/gateway/api/create")
-                .post(body)
-                .addHeader("Content-Type", "application/json")
-                .build();
-
-        // Thực hiện yêu cầu HTTP
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                // Xử lý lỗi khi gửi yêu cầu đến máy chủ của bạn
-                Log.d("erroRespon", "onFailure: ");
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                // Xử lý phản hồi từ máy chủ của bạn sau khi gửi thông tin thành công
-                Log.d("succesRespon", "onResponse: " + response.message());
-            }
-        });
-    }
-
+    /*------------------------------BEGIN-----------------------------------------*/
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -181,4 +156,108 @@ public class PaymentActivity extends Activity {
             tvMessage.setText("message: " + ("Khong thanh cong, vui long gui lai"));
         }
     }
+    /*------------------------------END-----------------------------------------*/
+
+    /*------------------------------BEGIN-----------------------------------------*/
+    private void sendPaymentInfoToServer() {
+        String signature = "";
+        Map<String, String> data = new TreeMap<>();
+        try {
+            // Dữ liệu mẫu
+            data.put("accessKey", "$accessKey"); // Missing Acesskey (Test)
+            data.put("amount", String.valueOf(eventValue.get(MoMoParameterNamePayment.AMOUNT)));
+            data.put("extraData", String.valueOf(eventValue.get(MoMoParameterNamePayment.EXTRA_DATA)));
+            data.put("ipnUrl", "https://6755-113-161-88-245.ngrok-free.app/");
+            data.put("orderId", String.valueOf(eventValue.get("orderId")));
+            data.put("orderInfo", "Rau ma");
+            data.put("partnerCode", String.valueOf(eventValue.get(MoMoParameterNamePayment.PARTNER_CODE)));
+            data.put("redirectUrl", "https://6755-113-161-88-245.ngrok-free.app/");
+            data.put("requestId", String.valueOf(eventValue.get(MoMoParameterNamePayment.REQUEST_ID)));
+            data.put("requestType", "captureWallet");
+            String secrectKey = "/Kdc/wHESkhvYKITgHfSeaXl35MNlzWjVYfVDLMdo1E"; //Missing SecretKey (Test)
+            signature = generateSignature(data, secrectKey);
+            Log.d("signature", "sendPaymentInfoToServer: "+ signature);
+        } catch (NoSuchAlgorithmException | InvalidKeyException e) {
+            e.printStackTrace();
+        }
+        // Tạo một yêu cầu HTTP POST để gửi thông tin đến máy chủ của bạn
+
+        OkHttpClient client = new OkHttpClient();
+        MediaType mediaType = MediaType.parse("application/json; charset=UTF-8");
+        JSONObject jsonObject = new JSONObject();
+        try {
+            // Đặt các thuộc tính cho đối tượng JSON
+            jsonObject.put("partnerCode", eventValue.get(MoMoParameterNamePayment.PARTNER_CODE));
+            jsonObject.put("requestId", eventValue.get(MoMoParameterNamePayment.REQUEST_ID));
+            jsonObject.put("amount", eventValue.get(MoMoParameterNamePayment.AMOUNT));
+            jsonObject.put("orderId", eventValue.get("orderId"));
+            jsonObject.put("orderInfo", "Rau ma");
+            jsonObject.put("redirectUrl", "https://6755-113-161-88-245.ngrok-free.app/");
+            jsonObject.put("ipnUrl","https://6755-113-161-88-245.ngrok-free.app/");
+            jsonObject.put("extraData", eventValue.get(MoMoParameterNamePayment.EXTRA_DATA));
+            jsonObject.put("lang","vi");
+            jsonObject.put("signature", signature.toString());
+            jsonObject.put("requestType", "captureWallet");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        Log.d("requestCode", "sendPaymentInfoToServer: " + jsonObject.toString());
+        RequestBody body = RequestBody.create(mediaType, jsonObject.toString());
+        Request request = new Request.Builder()
+                .url("https://test-payment.momo.vn/v2/gateway/api/create")
+                .post(body)
+                .addHeader("Content-Type", "application/json")
+                .build();
+
+        // Thực hiện yêu cầu HTTP
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                // Xử lý lỗi khi gửi yêu cầu đến máy chủ của bạn
+                Log.d("erroRespon", "onFailure: " + e.toString());
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                // Xử lý phản hồi từ máy chủ của bạn sau khi gửi thông tin thành công
+                Log.d("succesRespon", "onResponse: " + response.message());
+            }
+        });
+    }
+    /*------------------------------END-----------------------------------------*/
+
+    /*------------------------------BEGIN-----------------------------------------*/
+    public static String generateSignature(Map<String, String> data, String secretKey) throws NoSuchAlgorithmException, InvalidKeyException {
+        // Sắp xếp dữ liệu theo thứ tự a-z
+        TreeMap<String, String> sortedData = new TreeMap<>(data);
+
+        // Tạo chuỗi từ dữ liệu đã sắp xếp
+        StringBuilder message = new StringBuilder();
+        for (Map.Entry<String, String> entry : sortedData.entrySet()) {
+            message.append(entry.getKey()).append("=").append(entry.getValue()).append("&");
+        }
+        message.deleteCharAt(message.length() - 1); // Loại bỏ dấu & cuối cùng
+
+        // Tạo chuỗi ký tự từ dữ liệu
+        String messageString = message.toString();
+
+        // Sử dụng HMAC_SHA256 để tạo chữ ký
+        Mac sha256Hmac = Mac.getInstance("HmacSHA256");
+        SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
+        sha256Hmac.init(secretKeySpec);
+        byte[] signatureBytes = sha256Hmac.doFinal(messageString.getBytes(StandardCharsets.UTF_8));
+
+        // Chuyển đổi byte thành dạng hex
+        StringBuilder hexStringBuilder = new StringBuilder();
+        for (byte b : signatureBytes) {
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1) {
+                hexStringBuilder.append('0');
+            }
+            hexStringBuilder.append(hex);
+        }
+        return hexStringBuilder.toString();
+    }
+    /*------------------------------END-----------------------------------------*/
 }
