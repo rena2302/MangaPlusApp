0d6dfb7ec14e66fa55133a7337b089029b53d603
Phan Văn Năng <109004626+YunneeToiChoi@users.noreply.github.com>
2024-03-02 14:20:58 +0700

    build user Avater : update user Avt

--- app/src/main/java/com/example/mangaplusapp/Fragment/UserProfileFragment.java
+++ app/src/main/java/com/example/mangaplusapp/Fragment/UserProfileFragment.java
@@ -1,41 +1,48 @@
 package com.example.mangaplusapp.Fragment;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatButton;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.bumptech.glide.Glide;
 import com.example.mangaplusapp.Activity.LoginActivity;
-import com.example.mangaplusapp.Activity.TestAddMangaActivity;
-import com.example.mangaplusapp.Activity.UserEditActivity;
 import com.example.mangaplusapp.Helper.DBHelper.UserDBHelper;
 import com.example.mangaplusapp.R;
 
 public class UserProfileFragment extends Fragment {
-    ImageView TabDialog;
+    ImageView TabDialog,getUserAvtIMG;
     UserDBHelper db;
-TextView getUserNameInfoTxt,getUserNameTittleTxt,getUserEmailTxt,getUserPasswordTxt,HeaderEmail;
-int userId;
-String userEmail,userPassword,userName;
+    CardView avtContainer;
+    TextView getUserNameInfoTxt,getUserNameTittleTxt,getUserEmailTxt,getUserPasswordTxt,HeaderEmail;
+    int userId;
+    String userEmail,userPassword,userName,userAvt;
+    private static final int SELECT_IMAGE = 100;
+
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -55,6 +62,8 @@ String userEmail,userPassword,userName;
         getUserPasswordTxt = root.findViewById(R.id.userPassword_info);
         HeaderEmail=root.findViewById(R.id.userName_Email);
         TabDialog=root.findViewById(R.id.Tab_showdialog);
+        getUserAvtIMG = root.findViewById(R.id.Avatar_Profile_Image);
+        avtContainer = root.findViewById(R.id.Avatar_Profile_Card);
 
         TabDialog.setOnClickListener(v->{
             showDialog();
@@ -68,8 +77,10 @@ String userEmail,userPassword,userName;
         userName = db.getUserName(userId);
         userPassword = db.getUserPassword(userId);
         userEmail = db.getUserEmail(userId);
+        userAvt = db.getPicture(userId);
         if(userId!=-1){
             userExists();
+            userSetIMG();
         }
         else{
             handleNotLoggedIn();
@@ -104,6 +115,31 @@ String userEmail,userPassword,userName;
         getUserNameInfoTxt.setText(userName);
         getUserNameTittleTxt.setText(userName);
         HeaderEmail.setText(userEmail);
+        Glide.with(this).load(userAvt).into(getUserAvtIMG);
+    }
+    void userSetIMG(){
+        avtContainer.setOnClickListener(v->{
+            Intent intent = new Intent (Intent.ACTION_GET_CONTENT);
+            intent.setType("image/*");
+            startActivityForResult(
+                    Intent.createChooser(intent,"Select Image"),
+                    SELECT_IMAGE
+            );
+        });
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == SELECT_IMAGE && resultCode == Activity.RESULT_OK && data != null){
+            Uri uri = data.getData();
+            // Hiển thị hình ảnh được chọn
+            // Thêm dữ liệu vào cơ sở dữ liệu
+                Log.d("ADD URI ", "TRUE" + uri.toString());
+                db.updatePicture(userId,uri);
+                Glide.with(this).load(userAvt).into(getUserAvtIMG);
+                Toast.makeText(getActivity(), "Update Avatar Successful", Toast.LENGTH_SHORT).show();
+
+        }
     }
     private void showDialog()
     {
