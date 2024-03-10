b25c2bc3d84064c9a6bb0ff0c290ada254793cf2
rena2302 <khoivu2302@gmail.com>
2024-03-10 23:16:13 +0700

    User-FireBase

--- app/src/main/java/com/example/mangaplusapp/Activity/User/MangaDetailActivity.java
+++ app/src/main/java/com/example/mangaplusapp/Activity/User/MangaDetailActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,8 +14,13 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.mangaplusapp.Activity.Base.BaseActivity;
 import com.example.mangaplusapp.Adapter.ChapterAdapter;
+import com.example.mangaplusapp.R;
 import com.example.mangaplusapp.databinding.ActivityMangaDetailBinding;
 import com.example.mangaplusapp.object.Chapters;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -22,21 +28,34 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class MangaDetailActivity extends BaseActivity {
-
+    Intent intent;
+    FirebaseAuth firebaseAuth;
+    FirebaseUser currentUser;
     ActivityMangaDetailBinding binding;
     private List<Chapters> chapterList = new ArrayList<>();
     // Khởi tạo adapter trước khi hiển thi
     private ChapterAdapter chapterAdapter ;
+    String mangaId, nameManga, mangaPicture, mangaDescription;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMangaDetailBinding.inflate(getLayoutInflater());
+        intent = getIntent();
+        mangaId = intent.getStringExtra("ID_MANGA");
+        nameManga = intent.getStringExtra("NAME_MANGA");
+        mangaPicture = intent.getStringExtra("PICTURE_MANGA");
+        mangaDescription = intent.getStringExtra("DESCRIPTION_MANGA");
+        firebaseAuth = FirebaseAuth.getInstance();
+        currentUser = firebaseAuth.getCurrentUser();
         setContentView(binding.getRoot());
         setTextItem();
+        onClickEvent();
         loadChapters();
+        setFavorite();
     }
 
     private void loadChapters() {
@@ -47,7 +66,7 @@ public class MangaDetailActivity extends BaseActivity {
                 chapterList.clear();
                 for(DataSnapshot ds : snapshot.getChildren()){
                     Chapters chapter = ds.getValue(Chapters.class);
-                    if (chapter != null && chapter.getID_MANGA_CHAPTER().equals(getIntent().getExtras().getString("ID_MANGA"))) {
+                    if (chapter != null && chapter.getID_MANGA_CHAPTER().equals(mangaId)) {
                         chapterList.add(chapter);
                     }
                 }
@@ -65,10 +84,7 @@ public class MangaDetailActivity extends BaseActivity {
             }
         });
     }
-    private void setTextItem() {
-        Intent intent = getIntent();
-        Bundle extras = intent.getExtras();
-        binding.mangaDetailDescription.setText(extras.getString("DESCRIPTION_MANGA"));
+    private void onClickEvent(){
         binding.backDetailProductBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -80,7 +96,7 @@ public class MangaDetailActivity extends BaseActivity {
             public void onClick(View v) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(MangaDetailActivity.this);
                 builder.setTitle("Manga Description")
-                        .setMessage(extras.getString("DESCRIPTION_MANGA"))
+                        .setMessage(mangaDescription)
                         .setNegativeButton("Close", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
@@ -90,9 +106,111 @@ public class MangaDetailActivity extends BaseActivity {
                         .show();
             }
         });
-        binding.mangaDetailTitle.setText(extras.getString("NAME_MANGA"));
+        binding.mangaDetailFavorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleFavorite();
+            }
+        });
+    }
+    protected void addToFavorite(){
+        if(firebaseAuth.getCurrentUser() == null){
+            Toast.makeText(this,"You're not login", Toast.LENGTH_SHORT).show();
+            return;
+        }else {
+            long timestamp = System.currentTimeMillis();
+            HashMap<String,Object> hashMap = new HashMap<>();
+            hashMap.put("ID_MANGA", mangaId);
+            DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users");
+            reference.child(firebaseAuth.getUid()).child("Favorites").child(mangaId)
+                    .setValue(hashMap)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void unused) {
+                            Toast.makeText(MangaDetailActivity.this, "Add favorite successful", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
+
+    }
+    protected void removeFromFavorite(String mangaIdToRemove){
+        if(firebaseAuth.getCurrentUser() == null){
+            Toast.makeText(this,"You're not logged in", Toast.LENGTH_SHORT).show();
+            return;
+        }else {
+            DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users");
+            reference.child(firebaseAuth.getUid()).child("Favorites").child(mangaIdToRemove)
+                    .addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                dataSnapshot.getRef().removeValue()
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void unused) {
+                                                Toast.makeText(MangaDetailActivity.this, "Remove from favorite successful", Toast.LENGTH_SHORT).show();
+                                            }
+                                        })
+                                        .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Toast.makeText(MangaDetailActivity.this, "Failed to remove from favorite", Toast.LENGTH_SHORT).show();
+                                            }
+                                        });
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+                            Toast.makeText(MangaDetailActivity.this, "Database error occurred", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
+    }
+    private void toggleFavorite() {
+        DatabaseReference userFavoritesRef = FirebaseDatabase.getInstance().getReference("Users")
+                .child(currentUser.getUid()).child("Favorites");
+        userFavoritesRef.child(mangaId)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        dataSnapshot.getRef().getKey();
+                        if (dataSnapshot.exists()) {
+                            // Manga is already in favorites, remove it
+                            binding.mangaDetailFavorite.setImageResource(R.drawable.ic_favorite);
+                            removeFromFavorite(mangaId);
+                        } else {
+                            // Manga is not in favorites, add it
+                            binding.mangaDetailFavorite.setImageResource(R.drawable.ic_favorite_fill);
+                            addToFavorite();
+                        }
+                    }
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        // Handle error
+                    }
+                });
+    }
+    private void setFavorite(){
+        DatabaseReference userFavoritesRef = FirebaseDatabase.getInstance().getReference("Users")
+                .child(currentUser.getUid()).child("Favorites");
+        userFavoritesRef.child(mangaId)
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.exists()) {
+                            binding.mangaDetailFavorite.setImageResource(R.drawable.ic_favorite_fill);
+                        }
+                    }
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        // Handle error
+                    }
+                });
+    }
+    private void setTextItem() {
+        binding.mangaDetailDescription.setText(intent.getStringExtra("DESCRIPTION_MANGA"));
+        binding.mangaDetailTitle.setText(intent.getStringExtra("NAME_MANGA"));
         Glide.with(binding.mangaDetailImg)
-                .load(extras.getString("PICTURE_MANGA"))
+                .load(intent.getStringExtra("PICTURE_MANGA"))
                 .into(binding.mangaDetailImg);
     }
     @Override
--- app/src/main/java/com/example/mangaplusapp/Fragment/MangaListFragment.java
+++ app/src/main/java/com/example/mangaplusapp/Fragment/MangaListFragment.java
@@ -14,9 +14,12 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mangaplusapp.Adapter.TruyenTranhAdapter;
+import com.example.mangaplusapp.Database.User;
 import com.example.mangaplusapp.R;
 import com.example.mangaplusapp.object.Categories;
 import com.example.mangaplusapp.object.Mangas;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -36,10 +39,14 @@ public class MangaListFragment extends DialogFragment {
     public interface OnDataLoadedListener {
         void onDataLoaded(List<Mangas> truyenTranhList);
     }
+    FirebaseAuth mAuth;
+    FirebaseUser currentUser;
 
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
         // Khởi tạo adapter trước khi hiển thị dialog
         TruyenTranhAdapter adapter = new TruyenTranhAdapter(new ArrayList<>(), getContext());
 
@@ -50,15 +57,29 @@ public class MangaListFragment extends DialogFragment {
         RecyclerView recyclerView = view.findViewById(R.id.dashFragmentRcv);
         recyclerView.setLayoutManager(new GridLayoutManager(getContext(),2));
         recyclerView.setAdapter(adapter);
-        builder
-                .setView(view)
-                        .setTitle(category.getNAME_CATEGORY())
-                                .setPositiveButton("Close", new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        dialog.dismiss();
-                                    }
-                                });
+        if(category != null){
+            builder
+                    .setView(view)
+                    .setTitle(category.getNAME_CATEGORY())
+                    .setPositiveButton("Close", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+        }else {
+            builder
+                    .setView(view)
+                    .setTitle("Favorites")
+                    .setPositiveButton("Close", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+
+        }
+
 
         // Load dữ liệu và cập nhật adapter khi dữ liệu đã sẵn sàng
         loadMangas(new OnDataLoadedListener() {
@@ -73,24 +94,76 @@ public class MangaListFragment extends DialogFragment {
     }
 
     private void loadMangas(OnDataLoadedListener listener) {
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Mangas");
-        reference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                List<Mangas> truyenTranhList = new ArrayList<>();
-                for(DataSnapshot ds : snapshot.getChildren()){
-                    Mangas mangas = ds.getValue(Mangas.class);
-                    if (mangas != null && mangas.getID_CATEGORY_MANGA().equals(category.getID_CATEGORY())) {
-                        truyenTranhList.add(mangas);
+        if (category != null){
+            DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Mangas");
+            reference.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    List<Mangas> truyenTranhList = new ArrayList<>();
+                    for(DataSnapshot ds : snapshot.getChildren()){
+                        Mangas mangas = ds.getValue(Mangas.class);
+                        if (mangas != null && mangas.getID_CATEGORY_MANGA().equals(category.getID_CATEGORY())) {
+                            truyenTranhList.add(mangas);
+                        }
                     }
+                    // Gọi callback khi dữ liệu đã sẵn sàng
+                    listener.onDataLoaded(truyenTranhList);
                 }
-                // Gọi callback khi dữ liệu đã sẵn sàng
-                listener.onDataLoaded(truyenTranhList);
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    Toast.makeText(getContext(), "The loading mangas was interrupted",Toast.LENGTH_SHORT).show();
+                }
+            });
+        }else {
+            if (currentUser != null) {
+                String uid = currentUser.getUid();
+                DatabaseReference favoritesRef = FirebaseDatabase.getInstance().getReference("Users")
+                        .child(uid).child("Favorites");
+
+                favoritesRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        List<String> mangaIds = new ArrayList<>();
+                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                            String mangaId = snapshot.child("ID_MANGA").getValue(String.class);
+                            mangaIds.add(mangaId);
+                        }
+
+                        // Tạo một danh sách tạm thời để lưu trữ dữ liệu manga
+                        List<Mangas> mangasList = new ArrayList<>();
+                        DatabaseReference mangaRef = FirebaseDatabase.getInstance().getReference("Mangas");
+                        for (String mangaId : mangaIds) {
+                            mangaRef.orderByChild("ID_MANGA").equalTo(mangaId)
+                                    .addListenerForSingleValueEvent(new ValueEventListener() {
+                                        @Override
+                                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                            for (DataSnapshot mangaSnapshot : dataSnapshot.getChildren()) {
+                                                Mangas mangas = mangaSnapshot.getValue(Mangas.class);
+                                                mangasList.add(mangas);
+                                            }
+
+                                            // Kiểm tra xem đã tải tất cả dữ liệu chưa
+                                            if (mangasList.size() == mangaIds.size()) {
+                                                listener.onDataLoaded(mangasList);
+                                            }
+                                        }
+
+                                        @Override
+                                        public void onCancelled(@NonNull DatabaseError databaseError) {
+                                            // Xử lý lỗi nếu cần
+                                            Toast.makeText(getContext(), "The loading mangas was interrupted", Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        // Xử lý lỗi nếu cần
+                        Toast.makeText(getContext(), "The loading mangas was interrupted", Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                Toast.makeText(getContext(), "The loading mangas was interrupted",Toast.LENGTH_SHORT).show();
-            }
-        });
+        }
     }
 }
