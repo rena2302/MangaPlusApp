5e365b2c1d058e05a48883dac7d50df9b73d4cb3
Nguyễn Hoàng Phong <22dh112737@st.huflit.edu.vn>
2024-03-12 23:03:08 +0700

    UIPAYMENT

--- app/src/main/java/com/example/mangaplusapp/Activity/User/MangaDetailActivity.java
+++ app/src/main/java/com/example/mangaplusapp/Activity/User/MangaDetailActivity.java
@@ -2,13 +2,21 @@ package com.example.mangaplusapp.Activity.User;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
@@ -36,6 +44,7 @@ public class MangaDetailActivity extends BaseActivity {
     FirebaseAuth firebaseAuth;
     FirebaseUser currentUser;
     ActivityMangaDetailBinding binding;
+    AppCompatButton aaaa;
     private List<Chapters> chapterList = new ArrayList<>();
     // Khởi tạo adapter trước khi hiển thi
     private ChapterAdapter chapterAdapter ;
@@ -43,15 +52,26 @@ public class MangaDetailActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMangaDetailBinding.inflate(getLayoutInflater());
-        intent = getIntent();
-        mangaId = intent.getStringExtra("ID_MANGA");
-        nameManga = intent.getStringExtra("NAME_MANGA");
-        mangaPicture = intent.getStringExtra("PICTURE_MANGA");
-        mangaDescription = intent.getStringExtra("DESCRIPTION_MANGA");
-        firebaseAuth = FirebaseAuth.getInstance();
-        currentUser = firebaseAuth.getCurrentUser();
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_manga_detail);
+        aaaa=findViewById(R.id.Buy_Book);
+        aaaa.setOnClickListener(v->{
+            showdialog();
+        });
+    }
+    private void showdialog()
+    {
+        final Dialog dialog=new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.dialog_option_payment);
+        LinearLayout back=dialog.findViewById(R.id.BackToProduct);
+        back.setOnClickListener(v->{
+            dialog.dismiss();
+        });
+        dialog.show();
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().getAttributes().windowAnimations=R.style.DialogAnimation;
+        dialog.getWindow().setGravity(Gravity.BOTTOM);
     }
 
 
