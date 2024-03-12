24a091d3efc768c6af0a7b4b44c228cfa83f04f7
Nguyễn Hoàng Phong <22dh112737@st.huflit.edu.vn>
2024-03-12 23:12:51 +0700

    asdas

--- app/src/main/java/com/example/mangaplusapp/Activity/User/MangaDetailActivity.java
+++ app/src/main/java/com/example/mangaplusapp/Activity/User/MangaDetailActivity.java
@@ -1,62 +1,49 @@
 package com.example.mangaplusapp.Activity.User;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.widget.AppCompatButton;
-import androidx.recyclerview.widget.LinearLayoutManager;
-
-import android.app.Dialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.os.Bundle;
-import android.view.Gravity;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
-import android.widget.LinearLayout;
-import android.widget.Toast;
-
-import com.bumptech.glide.Glide;
-import com.example.mangaplusapp.Activity.Base.BaseActivity;
-import com.example.mangaplusapp.Adapter.ChapterAdapter;
-import com.example.mangaplusapp.R;
-import com.example.mangaplusapp.databinding.ActivityMangaDetailBinding;
-import com.example.mangaplusapp.object.Chapters;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+        import android.app.Dialog;
+        import android.content.Intent;
+        import android.graphics.Color;
+        import android.graphics.drawable.ColorDrawable;
+        import android.os.Bundle;
+        import android.view.Gravity;
+        import android.view.ViewGroup;
+        import android.view.Window;
+        import android.widget.LinearLayout;
+        import com.example.mangaplusapp.Activity.Base.BaseActivity;
+        import com.example.mangaplusapp.Adapter.ChapterAdapter;
+        import com.example.mangaplusapp.R;
+        import com.example.mangaplusapp.databinding.ActivityMangaDetailBinding;
+        import com.example.mangaplusapp.object.Chapters;
+        import com.google.firebase.auth.FirebaseAuth;
+        import com.google.firebase.auth.FirebaseUser;
+        import java.util.ArrayList;
+        import java.util.List;
 
 public class MangaDetailActivity extends BaseActivity {
     Intent intent;
     FirebaseAuth firebaseAuth;
     FirebaseUser currentUser;
     ActivityMangaDetailBinding binding;
-    AppCompatButton aaaa;
     private List<Chapters> chapterList = new ArrayList<>();
     // Khởi tạo adapter trước khi hiển thi
     private ChapterAdapter chapterAdapter ;
     String mangaId, nameManga, mangaPicture, mangaDescription;
+    private Boolean mangaPremium;
+    public interface OnPurchasedMangaIdsLoadedListener {
+        void onPurchasedMangaIdsLoaded(Boolean premium);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_manga_detail);
-        aaaa=findViewById(R.id.Buy_Book);
-        aaaa.setOnClickListener(v->{
-            showdialog();
-        });
+        binding = ActivityMangaDetailBinding.inflate(getLayoutInflater());
+        intent = getIntent();
+        mangaPremium = Boolean.parseBoolean(intent.getStringExtra("PREMIUM_MANGA"));
+        setContentView(binding.getRoot());
+        mangaId = intent.getStringExtra("ID_MANGA");
+        nameManga = intent.getStringExtra("NAME_MANGA");
+        mangaPicture = intent.getStringExtra("PICTURE_MANGA");
+        mangaDescription = intent.getStringExtra("DESCRIPTION_MANGA");
+        firebaseAuth = FirebaseAuth.getInstance();
+        currentUser = firebaseAuth.getCurrentUser();
     }
     private void showdialog()
     {
@@ -73,6 +60,4 @@ public class MangaDetailActivity extends BaseActivity {
         dialog.getWindow().getAttributes().windowAnimations=R.style.DialogAnimation;
         dialog.getWindow().setGravity(Gravity.BOTTOM);
     }
-
-
 }
